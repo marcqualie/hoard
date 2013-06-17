b63fe075bcbc971867216063dcda834729e1274f
Carl Markham <doddsey65@hotmail.com>
2013-06-18 00:03:01 +0100

    now returns bucket instance and added some exeption checking

--- lib/Silex/Provider/HoardServiceProvider.php
+++ lib/Silex/Provider/HoardServiceProvider.php
@@ -15,27 +15,30 @@ class HoardServiceProvider implements ServiceProviderInterface
 	{
 		$app['hoard'] = $app->share(function() use ($app) {
 
-			if (isset($app['hoard.server']))
+			if (!isset($app['hoard.server']))
 			{
-				$this->server = $app['hoard.server'];
+				throw new Exception('Invalid server configuration.');
 			}
 
-			if (isset($app['hoard.apikey']))
+			if (!isset($app['hoard.apikey']))
 			{
-				$this->apikey = $app['hoard.apikey'];
+				throw new Exception('A valid API Key must be provided.');
 			}
+
+			$this->apikey = $app['hoard.apikey'];
+			$this->server = $app['hoard.server'];
 
 			$this->instance = new \Hoard\Client(array(
 				'server' => $this->server,
 				'apikey' => $this->apikey
 			));
 
-			if (isset($app['hoard.bucket']))
+			if (!isset($app['hoard.bucket']))
 			{
-				$this->bucket = $this->instance->getBucket($app['hoard.bucket']);
+				throw new Exception('A valid bucket ID must be suplied.');
 			}
 
-			return $this->instance;
+			return $this->bucket = $this->instance->getBucket($app['hoard.bucket']);
 		});
 	}
 
