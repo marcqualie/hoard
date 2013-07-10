baf34ce1a0e48179807ac73ea602473d6b7472ec
Marc Qualie <marc@marcqualie.com>
2013-07-10 19:03:47 +0100

    API Key is now required for all api calls

--- app/Controller/Api.php
+++ app/Controller/Api.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Controller;
+use Model\User;
 
 class Api extends Base\Page {
 
@@ -8,13 +9,25 @@ class Api extends Base\Page {
     {
 
         // Get API Method
-        if ( ! isset($this->uri[1]))
-        {
+        if (! isset($this->uri[1])) {
             return $this->jsonError(500, 'No API Method Specified');
         }
         $api_method = $this->uri[1];
 
-        // TODO: Authenticate
+        // Authenticate using API Keys
+        $apikey = $this->app->request->get('apikey');
+        if (! $apikey) {
+            return $this->jsonError(401, 'API Key is required');
+        }
+        $user_collection = $this->app->mongo->selectCollection(User::$collection);
+        $apikey_isvalid = $user_collection->find(
+            array(
+                ('apikeys.' . $apikey . '.active') => true
+            )
+        )->count() > 0;
+        if (! $apikey_isvalid) {
+            return $this->jsonError(401, 'Invalid API Key');
+        }
 
         // Initialize API
         $api_controller_name = '\\Controller\\Api\\' . ucfirst($api_method);
@@ -33,6 +46,20 @@ class Api extends Base\Page {
         {
             return $this->jsonError($response->error['code'], $response->error['message']);
         }
+
+        // TODO: Log request
+        $user_collection->update(
+            array(
+                ('apikeys.' . $apikey . '.active') => true
+            ),
+            array(
+                '$inc' => array(
+                    ('apikeys.' . $apikey . '.requests') => 1
+                )
+            )
+        );
+
+        // Return Response Object
         return $this->json(
             $response->data,
             $response->code,
