30fe2c0647d0d4d3fcf36971b0a6ad8c1e8d546f
Marc Qualie <marc@marcqualie.com>
2013-08-12 13:08:03 +0100

    Make sure created buckets are stored in database correctly

--- tests/Hoard/Test/Bucket/CreateTest.php
+++ tests/Hoard/Test/Bucket/CreateTest.php
@@ -7,18 +7,43 @@ use Model\Bucket;
 class CreateTest extends TestCase
 {
 
+
+    /**
+     * Test that the model abstraction is working
+     */
     public function testCreateBucket()
     {
 
         $bucket_data = array(
             '_id' => 'test-bucket',
-            'name' => 'Test Bucket'
+            'description' => 'Test Bucket',
         );
         $bucket = new Bucket($bucket_data);
         $this->assertFalse(Bucket::exists($bucket->id));
         $bucket->save();
         $this->assertTrue(Bucket::exists($bucket->id));
 
+    }
+
+
+    /**
+     * Make sure the data is actually stored in the databse
+     */
+    public function testCreateBucketDataStored()
+    {
+        $bucket_data = array(
+            '_id' => 'test-bucket',
+            'description' => 'Test Bucket',
+        );
+        $collection = $this->mongo->selectCollection(Bucket::$collection);
+        $this->assertEquals(0, $collection->count());
+        $bucket = new Bucket($bucket_data);
+        $bucket->save();
+        $this->assertEquals(1, $collection->count());
+        $this->assertEquals(
+            array('description' => 'Test Bucket'),
+            $collection->findOne(array(), array('_id' => 0, 'description' => 1))
+        );
     }
 
 }
