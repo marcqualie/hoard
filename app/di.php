31ce33d8e7d47b8e9e5b76ddf996960d2bd9ee11
Marc Qualie <marc@marcqualie.com>
2014-07-12 18:37:05 +0100

    Split DI into it's own file

--- app/di.php
+++ app/di.php
@@ -1,5 +1,7 @@
 <?php
 
+$di = new Phalcon\DI\FactoryDefault();
+
 // Database Connection
 $di->set('mongo', function() {
     $mongo = new MongoClient("mongodb://localhost:27017");
@@ -8,3 +10,52 @@ $di->set('mongo', function() {
 $di->set('collectionManager', function(){
     return new Phalcon\Mvc\Collection\Manager();
 }, true);
+
+// Override Dispatcher
+$di->set('dispatcher', function() use ($di) {
+
+    // Obtain the standard eventsManager from the DI
+    $eventsManager = $di->getShared('eventsManager');
+
+    // Instantiate the Security plugin
+    $security = new Security($di);
+
+    // Listen for events produced in the dispatcher using the Security plugin
+    $eventsManager->attach('dispatch', $security);
+
+    $dispatcher = new Phalcon\Mvc\Dispatcher();
+
+    // Bind the EventsManager to the Dispatcher
+    $dispatcher->setEventsManager($eventsManager);
+
+    return $dispatcher;
+});
+
+// Routing
+$di->set('router', require '../app/router.php');
+
+// Start the session the first time a component requests the session service
+$di->set('session', function() {
+    $session = new Phalcon\Session\Adapter\Files();
+    $session->start();
+    return $session;
+});
+
+// Make flash messages use the sessions
+$di->set('flashSession', function() {
+    return new \Phalcon\Flash\Session([
+        'error' => 'alert alert-danger',
+        'success' => 'alert alert-success',
+        'notice' => 'alert alert-info'
+    ]);
+});
+
+// Setup the view component
+$di->set('view', function() use ($app_path) {
+    $view = new \Phalcon\Mvc\View();
+    $view->setViewsDir($app_path . '/views/');
+    return $view;
+});
+
+// Return instance
+return $di;
--- public/index.php
+++ public/index.php
@@ -14,54 +14,7 @@ try {
     ))->register();
 
     // Create a DI
-    $di = new Phalcon\DI\FactoryDefault();
-    require $app_path . '/di.php';
-
-    // Override Dispatcher
-    $di->set('dispatcher', function() use ($di) {
-
-        // Obtain the standard eventsManager from the DI
-        $eventsManager = $di->getShared('eventsManager');
-
-        // Instantiate the Security plugin
-        $security = new Security($di);
-
-        // Listen for events produced in the dispatcher using the Security plugin
-        $eventsManager->attach('dispatch', $security);
-
-        $dispatcher = new Phalcon\Mvc\Dispatcher();
-
-        // Bind the EventsManager to the Dispatcher
-        $dispatcher->setEventsManager($eventsManager);
-
-        return $dispatcher;
-    });
-
-    // Routing
-    $di->set('router', require '../app/router.php');
-
-    // Start the session the first time a component requests the session service
-    $di->set('session', function() {
-        $session = new Phalcon\Session\Adapter\Files();
-        $session->start();
-        return $session;
-    });
-
-    // Make flash messages use the sessions
-    $di->set('flashSession', function() {
-        return new \Phalcon\Flash\Session([
-            'error' => 'alert alert-danger',
-            'success' => 'alert alert-success',
-            'notice' => 'alert alert-info'
-        ]);
-    });
-
-    // Setup the view component
-    $di->set('view', function() use ($app_path) {
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir($app_path . '/views/');
-        return $view;
-    });
+    $di = require $app_path . '/di.php';
 
     // Handle the request
     $app = new \Phalcon\Mvc\Application($di);
