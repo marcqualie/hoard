74086e08c1552ade1d5c9071dff0f9ae37777bc2
Marc Qualie <marc@marcqualie.com>
2014-07-12 14:18:45 +0100

    Improve User model by adding timestamps and declaring fields explicitly

--- app/models/User.php
+++ app/models/User.php
@@ -3,9 +3,29 @@
 class User extends Phalcon\Mvc\Collection
 {
 
+    // Attributes
+    public $name;
+    public $username;
+    public $email;
+    public $password;
+    public $created_at;
+    public $updated_at;
+
     public function getSource()
     {
         return 'users';
+    }
+
+    public function beforeCreate()
+    {
+        if (! $this->created_at) {
+            $this->created_at = new MongoDate();
+        }
+    }
+
+    public function beforeSave()
+    {
+        $this->updated_at = new MongoDate();
     }
 
 }
