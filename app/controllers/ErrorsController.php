b77ca04b51ea3d702bd29c5dc186686df18a2e07
Marc Qualie <marc@marcqualie.com>
2014-07-12 12:29:20 +0100

    Disable main layout rendering on error pages

--- app/controllers/ErrorsController.php
+++ app/controllers/ErrorsController.php
@@ -1,7 +1,14 @@
 <?php
 
+use Phalcon\Mvc\View;
+
 class ErrorsController extends BaseController
 {
+
+    public function initialize()
+    {
+        $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
+    }
 
     public function notFoundAction()
     {
