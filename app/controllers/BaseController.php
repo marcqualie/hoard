e432fbe282a2fd53b2ccf93bc2b093d6d33787ec
Marc Qualie <marc@marcqualie.com>
2014-07-12 12:23:55 +0100

    Add functionality to BaseController for overriding default layout

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,4 +2,12 @@
 
 class BaseController extends Phalcon\Mvc\Controller
 {
+
+    protected $mainLayout = 'index';
+
+    public function initialize()
+    {
+        $this->view->setMainView('layouts/' . $this->mainLayout);
+    }
+
 }
